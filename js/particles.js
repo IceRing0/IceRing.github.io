9cc39101320ede92a794ed50fe7b0dfeae0d6389
2637560356@qq.com <2637560356@qq.com>
2019-08-01 18:55:40 +0800

    Site updated: 2019-08-01 18:55:37

--- js/particles.js
+++ js/particles.js
@@ -8,7 +8,7 @@ particlesJS("particles", {
             }
         },
         "color": {
-            "value": "#00adb5"
+            "value": "#6c00ff"
         },
         "shape": {
             "type": "circle",
@@ -48,7 +48,7 @@ particlesJS("particles", {
         "line_linked": {
             "enable": true,
             "distance": 150,
-            "color": "#00adb5",
+            "color": "#6c00ff",
             "opacity": 0.4,
             "width": 1
         },
